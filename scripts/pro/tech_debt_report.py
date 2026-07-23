a651b2dc11b14c8244a60f4504af40a4caa5e6b7
ramon-kaixo <barkaixo@gmail.com>
2026-07-23 14:45:03 +0200

    fix: tech_debt_report.py (broken by sed)

--- scripts/pro/tech_debt_report.py
+++ scripts/pro/tech_debt_report.py
@@ -1,21 +1,21 @@
-|#|!|/|u|s|r|/|b|i|n|/|e|n|v| |p|y|t|h|o|n|3|
-|"|"|"|G|e|n|e|r|a| |r|e|p|o|r|t|e| |d|e| |d|e|u|d|a| |t|e|c|n|i|c|a| |a|c|u|m|u|l|a|d|a|.|"|"|"|
-|i|m|p|o|r|t| |s|u|b|p|r|o|c|e|s|s|
-|i|m|p|o|r|t| |s|y|s|
-|f|r|o|m| |p|a|t|h|l|i|b| |i|m|p|o|r|t| |P|a|t|h|
-|
-|d|e|f| |r|u|n|(|c|m|d|)|:|
-| | | | |r|e|t|u|r|n| |s|u|b|p|r|o|c|e|s|s|.|r|u|n|(|c|m|d|,| |s|h|e|l|l|=|T|r|u|e|,| |c|a|p|t|u|r|e|_|o|u|t|p|u|t|=|T|r|u|e|,| |t|e|x|t|=|T|r|u|e|)|.|s|t|d|o|u|t|
-|
-|p|r|i|n|t|(|"|=|=|=| |T|E|C|H| |D|E|B|T| |R|E|P|O|R|T| |=|=|=|"|)|
-|t|o|d|o|_|c|o|u|n|t| |=| |r|u|n|(|'|g|r|e|p| |-|r|n| |"|T|O|D|O|"| |m|o|t|o|r|/| |-|-|i|n|c|l|u|d|e|=|*|.|p|y| ||| |w|c| |-|l|'|)|.|s|t|r|i|p|(|)|
-|f|i|x|m|e|_|c|o|u|n|t| |=| |r|u|n|(|'|g|r|e|p| |-|r|n| |"|F|I|X|M|E|"| |m|o|t|o|r|/| |-|-|i|n|c|l|u|d|e|=|*|.|p|y| ||| |w|c| |-|l|'|)|.|s|t|r|i|p|(|)|
-|e|x|c|_|c|o|u|n|t| |=| |r|u|n|(|'|g|r|e|p| |-|r|n| |"|e|x|c|e|p|t|:|.|*|p|a|s|s|"| |m|o|t|o|r|/| |-|-|i|n|c|l|u|d|e|=|*|.|p|y| ||| |g|r|e|p| |-|v| |"|_|_|p|y|c|a|c|h|e|_|_|"| ||| |w|c| |-|l|'|)|.|s|t|r|i|p|(|)|
-|r|u|f|f|_|c|o|u|n|t| |=| |r|u|n|(|'|c|d| |/|h|o|m|e|/|r|a|m|o|n|/|U|R|A|/|u|r|a|_|i|a|_|1|9|7|2| |&|&| |.|v|e|n|v|/|b|i|n|/|r|u|f|f| |c|h|e|c|k| |m|o|t|o|r|/| |-|-|o|u|t|p|u|t|-|f|o|r|m|a|t|=|c|o|n|c|i|s|e| |2|>|&|1| ||| |w|c| |-|l|'|)|.|s|t|r|i|p|(|)|
-|p|r|i|n|t|(|f|"|T|O|D|O| |c|o|m|m|e|n|t|s|:| |{|t|o|d|o|_|c|o|u|n|t|}|"|)|
-|p|r|i|n|t|(|f|"|F|I|X|M|E| |c|o|m|m|e|n|t|s|:| |{|f|i|x|m|e|_|c|o|u|n|t|}|"|)|
-|p|r|i|n|t|(|f|"|e|x|c|e|p|t|:|.|*|p|a|s|s|:| |{|e|x|c|_|c|o|u|n|t|}|"|)|
-|p|r|i|n|t|(|f|"|R|u|f|f| |e|r|r|o|r|s|:| |{|r|u|f|f|_|c|o|u|n|t|}|"|)|
-|f|o|r| |l|i|n|e| |i|n| |r|u|n|(|"|g|r|e|p| |-|r|n| |'|F|I|X|M|E|||T|O|D|O|'| |m|o|t|o|r|/| |-|-|i|n|c|l|u|d|e|=|*|.|p|y| ||| |h|e|a|d| |-|2|0|"|)|.|s|p|l|i|t|(|"|n|"|)|:|
-| | | | |i|f| |l|i|n|e|.|s|t|r|i|p|(|)|:|
-| | | | | | | | |p|r|i|n|t|(|f|"| | |{|l|i|n|e|.|s|t|r|i|p|(|)|}|"|)|
+#!/usr/bin/env python3
+"""Genera reporte de deuda tecnica acumulada."""
+import subprocess
+from pathlib import Path
+
+BASE = Path.home() / "URA" / "ura_ia_1972"
+
+
+def run(cmd):
+    return subprocess.run(cmd, shell=True, capture_output=True, text=True, cwd=str(BASE)).stdout
+
+
+print("=== TECH DEBT REPORT ===")
+todo = run('grep -rn "TODO" motor/ --include=*.py | wc -l').strip()
+fixme = run('grep -rn "FIXME" motor/ --include=*.py | wc -l').strip()
+exc = run('grep -rn "except:.*pass" motor/ --include=*.py | grep -v __pycache__ | wc -l').strip()
+ruff = run('.venv/bin/ruff check motor/ --output-format=concise 2>&1 | wc -l').strip()
+print(f"TODO comments: {todo}")
+print(f"FIXME comments: {fixme}")
+print(f"except:.*pass: {exc}")
+print(f"Ruff errors: {ruff}")
